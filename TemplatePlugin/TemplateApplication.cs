c2f72f29013666163941cef73be85323cd7d7bf0
XeroxDev <admin@xeroxdev.de>
2021-12-15 09:52:05 +0100

    Initial commit

---
+++ TemplatePlugin/TemplateApplication.cs
@@ -0,0 +1,16 @@
+namespace Loupedeck.TemplatePlugin
+{
+    using System;
+
+    public class TemplateApplication : ClientApplication
+    {
+        public TemplateApplication()
+        {
+
+        }
+
+        protected override String GetProcessName() => "";
+
+        protected override String GetBundleName() => "";
+    }
+}
